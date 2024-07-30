d2571c6920bc52fd5ce35b3e6194600ee5570b18
chaitany mishra <mr.chetan105@gmail.com>
2024-07-30 20:11:13 +0530

    first commit

---
+++ chapter02.py
@@ -0,0 +1,34 @@
+# pratice set number 01
+
+a = int(input("Enter 1st digit: "))
+b = int(input("enter 2nd digit: "))
+print("sum of the number is: " , a + b)
+
+# pratice question number 02
+
+a = float(input("enter number: "))
+b = float(input("enter 2nd number: "))
+c = a%b
+print("remender off the number is: " , c)
+
+# pratice question 03
+
+a =( input("enter the value you wnat's to see thear type: "))
+
+print(type(a))
+
+# question 04 
+
+a = int(input("Enter the first value: "))
+b = int(input("Enter the 2nd value: "))
+print(a , "is greater then " , b , "is: ", a>=b)
+
+# question 5
+a = int(input("enter the 1st number: "))
+b = int(input("enter the 2nd number: "))
+print("avrage of  the number is:  " , (a+b)/2)
+
+# question 05
+
+a = int(input("enter the value: "))
+print("square of the number is: ", a*a)
