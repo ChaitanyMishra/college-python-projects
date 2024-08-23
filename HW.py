77dd42ab3780b2524834c5de89199bd2c02ca09d
chaitany mishra <mr.chetan105@gmail.com>
2024-08-23 21:41:13 +0530

    homework

---
+++ HW.py
@@ -0,0 +1,78 @@
+# Question 1
+print("Hello World")
+
+# Question 2
+# Taking two numbers from the user
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+result = num1 + num2
+
+
+print("sum: ", num1, "and", num2, "is", result)
+print("\n")
+
+# Question 3
+# Taking two numbers from the user
+
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+print("Addition:", num1, "+", num2, "=", addition)
+print("\n")
+print("Subtraction:", num1, "-", num2, "=", subtraction)
+print("\n")
+print("Multiplication:", num1, "*", num2, "=", multiplication)
+print("\n")
+if num2 == 0:
+    print("Zero devision ERROR! ")
+else:
+    print("Division: ", num1 / num2)
+    print("\n")
+
+# Question 4
+# Taking length and width from the user and calculate the area of rectangle
+
+length = float(input("Enter the length of the rectangle: "))
+
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+print("area of the rectangle : ", area)
+print("\n")
+# Question 5
+# Taking base and height from the user
+
+base = float(input("Enter the base of the triangle: "))
+
+height = float(input("Enter the height of the triangle: "))
+
+area = 0.5 * base * height
+
+print("area of the triangle ", area)
+
+# Question 6
+# Taking length, width, and height from the user
+
+length = float(input("Enter the length of the cuboid: "))
+
+width = float(input("Enter the width of the cuboid: "))
+
+height = float(input("Enter the height of the cuboid: "))
+
+surface_area = 2 * (length * width + width * height + height * length)
+
+print("surface area of the cuboid : ", surface_area)
+
+# Question 7
+# Taking the lengths of the parallel sides and the height from the user
+
+
+# i don't know how to solve this!
